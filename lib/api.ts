b61f0bb062b0d86c8325b3d16204c4f41a00f707
linetgitonga <lintgitonga55@gmail.com>
2025-07-17 09:44:24 +0300

    base url

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "http://127.0.0.1:8000/api"
+// const BASE_URL = "http://127.0.0.1:8000/api"
+const BASE_URL = "https://mamascan-backend.onrender.com/api"
 
 // Types based on your API documentation
 export interface User {
